e6d8560bd3438011dad9e22dbf37932b5deffc18
Yachang Hassang Industry <yhassang9@gmail.com>
2025-08-27 03:22:35 -0500

    Update engine.js

--- project-root/public/engine.js
+++ project-root/public/engine.js
@@ -300,27 +300,3 @@ publishBtn.addEventListener("click", () => {
   .then(data => alert(data.message))
   .catch(err => alert("Error sending files: " + err));
 });
-// --- Auto-save to temporary memory ---
-function saveToTemporaryMemory() {
-  const iframeDoc = previewFrame.contentDocument || previewFrame.contentWindow.document;
-  const htmlContent = iframeDoc.documentElement.outerHTML;
-
-  fetch("/update", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      filename: "homepage.html", // or whichever file is active
-      content: htmlContent
-    })
-  }).catch(err => console.error("Temp memory save failed:", err));
-}
-
-// Optional: auto-save every 5 seconds
-setInterval(saveToTemporaryMemory, 5000);
-
-// Or: save whenever you make an edit
-function saveHistoryAndTempMemory() {
-  saveHistory();
-  saveToTemporaryMemory();
-}
-
